de7f1176d7e46702219782da2f82828aedb8a791
hexachris <cajipe.chriscaguin@gmail.com>
2024-12-30 21:08:07 +0800

    added arrays

--- SMARTGradeTracker/scoreEntry.Designer.cs
+++ SMARTGradeTracker/scoreEntry.Designer.cs
@@ -35,7 +35,7 @@
             this.SideBtn_userGuide = new System.Windows.Forms.PictureBox();
             this.Btn_add = new System.Windows.Forms.PictureBox();
             this.SubjectComboBox = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.AssementComboBox = new System.Windows.Forms.ComboBox();
             this.rawScoreBox = new System.Windows.Forms.TextBox();
             this.totalScoreBox = new System.Windows.Forms.TextBox();
             this.pointBox = new System.Windows.Forms.TextBox();
@@ -52,9 +52,10 @@
             this.Btn_Home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Btn_Home.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Btn_Home.Image = global::SMARTGradeTracker.Properties.Resources.home;
-            this.Btn_Home.Location = new System.Drawing.Point(57, 64);
+            this.Btn_Home.Location = new System.Drawing.Point(38, 43);
+            this.Btn_Home.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Btn_Home.Name = "Btn_Home";
-            this.Btn_Home.Size = new System.Drawing.Size(64, 64);
+            this.Btn_Home.Size = new System.Drawing.Size(43, 43);
             this.Btn_Home.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Btn_Home.TabIndex = 2;
             this.Btn_Home.TabStop = false;
@@ -68,9 +69,10 @@
             this.SideBtn_systemCredits.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.SideBtn_systemCredits.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SideBtn_systemCredits.Image = global::SMARTGradeTracker.Properties.Resources.sidebbtn_system_credits;
-            this.SideBtn_systemCredits.Location = new System.Drawing.Point(60, 471);
+            this.SideBtn_systemCredits.Location = new System.Drawing.Point(40, 314);
+            this.SideBtn_systemCredits.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SideBtn_systemCredits.Name = "SideBtn_systemCredits";
-            this.SideBtn_systemCredits.Size = new System.Drawing.Size(170, 44);
+            this.SideBtn_systemCredits.Size = new System.Drawing.Size(113, 29);
             this.SideBtn_systemCredits.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.SideBtn_systemCredits.TabIndex = 3;
             this.SideBtn_systemCredits.TabStop = false;
@@ -84,9 +86,10 @@
             this.SideBtn_gradeViewer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.SideBtn_gradeViewer.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SideBtn_gradeViewer.Image = global::SMARTGradeTracker.Properties.Resources.sidebtn_grade_viewer;
-            this.SideBtn_gradeViewer.Location = new System.Drawing.Point(53, 288);
+            this.SideBtn_gradeViewer.Location = new System.Drawing.Point(35, 192);
+            this.SideBtn_gradeViewer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SideBtn_gradeViewer.Name = "SideBtn_gradeViewer";
-            this.SideBtn_gradeViewer.Size = new System.Drawing.Size(164, 48);
+            this.SideBtn_gradeViewer.Size = new System.Drawing.Size(109, 32);
             this.SideBtn_gradeViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.SideBtn_gradeViewer.TabIndex = 4;
             this.SideBtn_gradeViewer.TabStop = false;
@@ -100,9 +103,10 @@
             this.SideBtn_userGuide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.SideBtn_userGuide.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SideBtn_userGuide.Image = global::SMARTGradeTracker.Properties.Resources.sidebtn_user_guide;
-            this.SideBtn_userGuide.Location = new System.Drawing.Point(70, 381);
+            this.SideBtn_userGuide.Location = new System.Drawing.Point(47, 254);
+            this.SideBtn_userGuide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SideBtn_userGuide.Name = "SideBtn_userGuide";
-            this.SideBtn_userGuide.Size = new System.Drawing.Size(132, 44);
+            this.SideBtn_userGuide.Size = new System.Drawing.Size(88, 29);
             this.SideBtn_userGuide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.SideBtn_userGuide.TabIndex = 5;
             this.SideBtn_userGuide.TabStop = false;
@@ -116,9 +120,10 @@
             this.Btn_add.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Btn_add.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Btn_add.Image = global::SMARTGradeTracker.Properties.Resources.btn_hover_add1;
-            this.Btn_add.Location = new System.Drawing.Point(414, 845);
+            this.Btn_add.Location = new System.Drawing.Point(276, 563);
+            this.Btn_add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Btn_add.Name = "Btn_add";
-            this.Btn_add.Size = new System.Drawing.Size(350, 51);
+            this.Btn_add.Size = new System.Drawing.Size(233, 34);
             this.Btn_add.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Btn_add.TabIndex = 6;
             this.Btn_add.TabStop = false;
@@ -133,67 +138,77 @@
             this.SubjectComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SubjectComboBox.ForeColor = System.Drawing.Color.MidnightBlue;
             this.SubjectComboBox.FormattingEnabled = true;
-            this.SubjectComboBox.Location = new System.Drawing.Point(442, 289);
+            this.SubjectComboBox.Location = new System.Drawing.Point(295, 193);
+            this.SubjectComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SubjectComboBox.Name = "SubjectComboBox";
-            this.SubjectComboBox.Size = new System.Drawing.Size(568, 28);
+            this.SubjectComboBox.Size = new System.Drawing.Size(380, 21);
             this.SubjectComboBox.Sorted = true;
             this.SubjectComboBox.TabIndex = 7;
             this.SubjectComboBox.Text = "Yuh";
+            this.SubjectComboBox.SelectedIndexChanged += new System.EventHandler(this.SubjectComboBox_SelectedIndexChanged);
             // 
-            // comboBox1
+            // AssementComboBox
             // 
-            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(442, 389);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(568, 28);
-            this.comboBox1.Sorted = true;
-            this.comboBox1.TabIndex = 8;
-            this.comboBox1.Text = "Yuh";
+            this.AssementComboBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AssementComboBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.AssementComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AssementComboBox.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.AssementComboBox.FormattingEnabled = true;
+            this.AssementComboBox.Location = new System.Drawing.Point(295, 259);
+            this.AssementComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AssementComboBox.Name = "AssementComboBox";
+            this.AssementComboBox.Size = new System.Drawing.Size(380, 21);
+            this.AssementComboBox.Sorted = true;
+            this.AssementComboBox.TabIndex = 8;
+            this.AssementComboBox.Text = "Yuh";
+            this.AssementComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // rawScoreBox
             // 
             this.rawScoreBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.rawScoreBox.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.rawScoreBox.Location = new System.Drawing.Point(442, 613);
+            this.rawScoreBox.Location = new System.Drawing.Point(295, 409);
+            this.rawScoreBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rawScoreBox.Name = "rawScoreBox";
-            this.rawScoreBox.Size = new System.Drawing.Size(226, 26);
+            this.rawScoreBox.Size = new System.Drawing.Size(152, 20);
             this.rawScoreBox.TabIndex = 9;
+            this.rawScoreBox.TextChanged += new System.EventHandler(this.rawScoreBox_TextChanged);
             // 
             // totalScoreBox
             // 
             this.totalScoreBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.totalScoreBox.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.totalScoreBox.Location = new System.Drawing.Point(783, 612);
+            this.totalScoreBox.Location = new System.Drawing.Point(522, 408);
+            this.totalScoreBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.totalScoreBox.Name = "totalScoreBox";
-            this.totalScoreBox.Size = new System.Drawing.Size(226, 26);
+            this.totalScoreBox.Size = new System.Drawing.Size(152, 20);
             this.totalScoreBox.TabIndex = 10;
+            this.totalScoreBox.TextChanged += new System.EventHandler(this.totalScoreBox_TextChanged);
             // 
             // pointBox
             // 
             this.pointBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pointBox.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.pointBox.Location = new System.Drawing.Point(442, 725);
+            this.pointBox.Location = new System.Drawing.Point(295, 483);
+            this.pointBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pointBox.Name = "pointBox";
-            this.pointBox.Size = new System.Drawing.Size(568, 26);
+            this.pointBox.Size = new System.Drawing.Size(380, 20);
             this.pointBox.TabIndex = 11;
+            this.pointBox.TextChanged += new System.EventHandler(this.pointBox_TextChanged);
             // 
             // scoreEntry
             // 
             this.AccessibleDescription = "Your Personal Grade Tracker";
             this.AccessibleName = "SMART";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackgroundImage = global::SMARTGradeTracker.Properties.Resources.background4;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(1478, 1054);
+            this.ClientSize = new System.Drawing.Size(913, 499);
             this.Controls.Add(this.pointBox);
             this.Controls.Add(this.totalScoreBox);
             this.Controls.Add(this.rawScoreBox);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AssementComboBox);
             this.Controls.Add(this.SubjectComboBox);
             this.Controls.Add(this.Btn_add);
             this.Controls.Add(this.SideBtn_userGuide);
@@ -203,9 +218,10 @@
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1500, 1110);
-            this.MinimumSize = new System.Drawing.Size(1500, 1110);
+            this.MaximumSize = new System.Drawing.Size(1005, 753);
+            this.MinimumSize = new System.Drawing.Size(915, 524);
             this.Name = "scoreEntry";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SMART • Score Entry";
@@ -227,7 +243,7 @@
         private System.Windows.Forms.PictureBox SideBtn_userGuide;
         private System.Windows.Forms.PictureBox Btn_add;
         private System.Windows.Forms.ComboBox SubjectComboBox;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox AssementComboBox;
         private System.Windows.Forms.TextBox rawScoreBox;
         private System.Windows.Forms.TextBox totalScoreBox;
         private System.Windows.Forms.TextBox pointBox;
--- SMARTGradeTracker/scoreEntry.cs
+++ SMARTGradeTracker/scoreEntry.cs
@@ -15,6 +15,39 @@ namespace SMARTGradeTracker
         public scoreEntry()
         {
             InitializeComponent();
+            this.Load += ScoreEntry_Load;
+        }
+
+        private void ScoreEntry_Load(object sender, EventArgs e) // dito placement ng mga pang add ng values sa mga combo box etc.
+        {
+            
+            SubjectComboBox.Items.Add("ITEC 104 - Data Structures and Algorithm");
+            SubjectComboBox.Items.Add("ITEC 105 - Information Management");
+            SubjectComboBox.Items.Add("CMSC 202 - Discrete Structures 2");
+            SubjectComboBox.Items.Add("CMSC 203 - Object Oriented Programming");
+            SubjectComboBox.Items.Add("GEC 106 - Art Appreciation");
+            SubjectComboBox.Items.Add("SOSLIT - Sosyedad at Literatura");
+            SubjectComboBox.Items.Add("MATH 24 - Calculus");
+            SubjectComboBox.Items.Add("PATHFit 3");
+
+            
+            AssementComboBox.Items.Add("20% Midterms");
+            AssementComboBox.Items.Add("20% Finals");
+            AssementComboBox.Items.Add("20% Quiz");
+            AssementComboBox.Items.Add("20% Activities");
+            AssementComboBox.Items.Add("15% Participation");
+            AssementComboBox.Items.Add("5% Attendance");
+
+            
+            this.rawScoreBox.KeyPress += new KeyPressEventHandler(this.rawScoreBox_KeyPress);
+        }
+
+        private void rawScoreBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) // Check if "Enter" key is pressed
+            {
+                Btn_add_Click(sender, e); // Trigger the button click event to store grades
+            }
         }
 
         private void Btn_Home_Click(object sender, EventArgs e)
@@ -51,13 +84,13 @@ namespace SMARTGradeTracker
             form.Show();
         }
 
-
         private void SideBtn_userGuide_Click(object sender, EventArgs e)
         {
             this.Hide();
             userGuide form = new userGuide();
             form.Show();
         }
+
         private void SideBtn_userGuide_MouseEnter(object sender, EventArgs e)
         {
             SideBtn_userGuide.Image = SMARTGradeTracker.Properties.Resources.sidebtn_hover_user_guide;
@@ -87,7 +120,21 @@ namespace SMARTGradeTracker
 
         private void Btn_add_Click(object sender, EventArgs e) // This is the "Add to Database" button
         {
+            
+            if (string.IsNullOrEmpty(rawScoreBox.Text) || string.IsNullOrEmpty(totalScoreBox.Text))
+            {
+                MessageBox.Show("Please enter values in all fields.");
+                return;
+            }
 
+            
+            decimal rawScore = decimal.Parse(rawScoreBox.Text);
+            decimal totalScore = decimal.Parse(totalScoreBox.Text);
+            decimal points = string.IsNullOrEmpty(pointBox.Text) ? 0 : decimal.Parse(pointBox.Text);
+
+            // Store values in the arrays or perform further logic if meron na
+            // Example: storing the score (you can replace this with actual array storage logic)
+            MessageBox.Show($"Raw Score: {rawScore}, Total Score: {totalScore}, Additional Points: {points}");
         }
 
         private void Btn_add_MouseEnter(object sender, EventArgs e)
@@ -98,6 +145,31 @@ namespace SMARTGradeTracker
         private void Btn_add_MouseLeave(object sender, EventArgs e)
         {
             Btn_add.Image = SMARTGradeTracker.Properties.Resources.btn_hover_add1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // eto pala yung assessmentcombobox
+        {
+
+        }
+
+        private void SubjectComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rawScoreBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pointBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalScoreBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
